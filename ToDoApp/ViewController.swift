28bbaa92da300ab94f992d444be9c131a94150b3
jkim68888 <jkim68888@gmail.com>
2022-08-08 22:34:46 +0900

    storyboard 삭제, 루트뷰컨 설정

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by 김지현 on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+    }
+
+
+}
+

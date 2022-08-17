b0b54e8073349ab0dd11dbe16da8b965156d5ced
jkim68888 <jkim68888@gmail.com>
2022-08-17 21:11:45 +0900

    editviewcontroller 수정

--- ToDoApp/Scene/Edit/ViewController/EditViewController.swift
+++ ToDoApp/Scene/Edit/ViewController/EditViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class EditViewController: UIViewController {
+    
+    private let headerView = HeaderView()
+    
+    private let editTasksTableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUI() {
+        
     }
-    */
-
 }
